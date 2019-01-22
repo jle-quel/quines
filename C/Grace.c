c8feecb7cebfa6061345e72a0b70247ffb05fcea
jle-quel <jle-quel@student.42.us.org>
2019-01-22 19:44:53 +0100

    [ADD]: Sully.c

--- C/Grace.c
+++ C/Grace.c
@@ -3,16 +3,19 @@
 #include <unistd.h>
 
 # define FAILURE -1
-# define STR "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$c# define FAILURE -1%1$c# define STR %3$c%4$s%3$c%1$c# define FT(x)int main(void){x}%1$c%1$cFT%1$c(%1$cint fd = 0;%1$c%1$cif ((fd = open(%3$cGrace_kid.c%3$c, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == FAILURE)%1$c%2$creturn FAILURE;%1$cchar *str = STR;%1$cdprintf(fd, str, 0xa, 0x9, 0x22, str);%1$cclose(fd);%1$c)%1$c"
-# define FT(x)int main(void){x}
+# define STR "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c%1$c# define FAILURE -1%1$c# define STR %3$c%4$s%3$c%1$c# define FT(x) int main(void){x}%1$c%1$cFT%1$c(%1$c%2$cint fd = 0;%1$c%2$cchar *str = STR;%1$c%1$c%2$cif ((fd = open(%3$cGrace_kid.c%3$c, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == FAILURE)%1$c%2$c%2$creturn FAILURE;%1$c%1$c%2$cdprintf(fd, str, 0xa, 0x9, 0x22, str);%1$c%2$cclose(fd);%1$c%1$c%2$creturn 0;%1$c)%1$c"
+# define FT(x) int main(void){x}
 
 FT
 (
-int fd = 0;
+	int fd = 0;
+	char *str = STR;
 
-if ((fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == FAILURE)
-	return FAILURE;
-char *str = STR;
-dprintf(fd, str, 0xa, 0x9, 0x22, str);
-close(fd);
+	if ((fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == FAILURE)
+		return FAILURE;
+
+	dprintf(fd, str, 0xa, 0x9, 0x22, str);
+	close(fd);
+
+	return 0;
 )
---
+++ C/Sully.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int fd = 0;
+	int index = 5;
+	char file[10];
+	char cmd[40];
+	char *str = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$c%1$cint main(void)%1$c{%1$c%2$cint fd = 0;%1$c%2$cint index = %5$d;%1$c%2$cchar file[10];%1$c%2$cchar cmd[40];%1$c%2$cchar *str = %3$c%6$s%3$c;%1$c%1$c%2$cif (index < 0)%1$c%2$c%2$creturn 0;%1$C%1$c%2$csprintf(file, %3$cSully_%4$cd.c%3$c, index);%1$c%2$csprintf(cmd, %3$cgcc Sully_%4$cd.c && ./a.out ; rm -f a.out%3$c, index);%1$c%1$c%2$cif ((fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)%1$c%2$c%2$creturn -1;%1$c%1$c%2$cdprintf(fd, str, 0xa, 0x9, 0x22, 0x25, index - 1, str);%1$c%2$cclose(fd);%1$c%1$c%2$csystem(cmd);%1$c%1$c%2$creturn 0;%1$c}%1$c";
+
+	if (index < 0)
+		return 0;
+
+	sprintf(file, "Sully_%d.c", index);
+	sprintf(cmd, "gcc Sully_%d.c && ./a.out ; rm -f a.out", index);
+
+	if ((fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)
+		return -1;
+
+	dprintf(fd, str, 0xa, 0x9, 0x22, 0x25, index - 1, str);
+	close(fd);
+
+	system(cmd);
+
+	return 0;
+}
