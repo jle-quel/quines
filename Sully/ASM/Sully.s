8a56428e758647ef5a78f0ed17cd8335e3e67f98
jle-quel <jle-quel@student.42.us.org>
2019-01-25 11:26:24 +0100

    [UPDATE]: secure buffer

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -1,11 +1,11 @@
 section .bss
-	filebuf resb 10
-	cmdbuf resb 60
+	filebuf resb 4096
+	cmdbuf resb 4096
 
 section .data
 	filename db "Sully_%d.s", 0x0
 	cmdname db "nasm -f macho64 Sully_%1$d.s && gcc Sully_%1$d.o -o Sully_%1$d && rm Sully_%1$d.o && ./Sully_%1$d", 0x0
-	string db "section .bss%1$c%2$cfilebuf resb 10%1$c%2$ccmdbuf resb 60%1$c%1$csection .data%1$c%2$cfilename db %3$cSully_%%d.s%3$c, 0x0%1$c%2$ccmdname db %3$cnasm -f macho64 Sully_%%1$d.s && gcc Sully_%%1$d.o -o Sully_%%1$d && rm Sully_%%1$d.o && ./Sully_%%1$d%3$c, 0x0%1$c%2$cstring db %3$c%5$s%3$c, 0x0%1$c%1$csection .text%1$c%2$cglobal _main%1$c%2$cextern _sprintf%1$c%2$cextern _dprintf%1$c%2$cextern _system%1$c%1$c_main:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cpush rdi%1$c%2$cpush rsi%1$c%2$cpush rdx%1$c%2$cpush rcx%1$c%2$cpush r8%1$c%2$cpush r12%1$c%2$cpush r13%1$c%1$c%2$cmov r12, %4$d%1$c%1$c%2$ccmp r12d, -1%1$c%2$cje end%1$c%1$c%2$csub rsp, 56%1$c%2$clea rdi, [ rel filebuf ]%1$c%2$clea rsi, [ rel filename ]%1$c%2$cmov rdx, r12%1$c%2$ccall _sprintf%1$c%2$cadd rsp, 56%1$c%1$c%2$csub rsp, 56%1$c%2$clea rdi, [ rel cmdbuf ]%1$c%2$clea rsi, [ rel cmdname ]%1$c%2$cmov rdx, r12%1$c%2$ccall _sprintf%1$c%2$cadd rsp, 56%1$c%1$c%2$clea rdi, [ rel filebuf ]%1$c%2$cmov rsi, 1537%1$c%2$cmov rdx, 0644o%1$c%2$cmov rax, 0x2000005%1$c%2$csyscall%1$c%2$cjc error%1$c%2$cmov r13, rax%1$c%1$c%2$csub rsp, 64%1$c%2$cmov rdi, r13%1$c%2$clea rsi, [ rel string ]%1$c%2$cmov rdx, 0xa%1$c%2$cmov rcx, 0x9%1$c%2$cmov r8, 0x22%1$c%2$cmov r9, r12%1$c%2$cdec r9%1$c%2$cpush rsi%1$c%2$ccall _dprintf%1$c%2$cadd rsp, 64%1$c%1$c%2$cmov rdi, r13%1$c%2$cmov rax, 0x2000006%1$c%2$csyscall%1$c%2$cjc error%1$c%1$c%2$clea rdi, [ rel cmdbuf ]%1$c%2$ccall _system%1$c%1$c%2$cpop r13%1$c%2$cpop r12%1$c%2$cpop r8%1$c%2$cpop rcx%1$c%2$cpop rdx%1$c%2$cpop rsi%1$c%2$cpop rdi%1$c%1$cend:%1$c%2$cxor rax, rax%1$c%1$c%2$cleave%1$c%2$cret%1$c%1$cerror:%1$c%2$cmov rax, -1%1$c%1$c%2$cleave%1$c%2$cret%1$c", 0x0
+	string db "section .bss%1$c%2$cfilebuf resb 4096%1$c%2$ccmdbuf resb 4096%1$c%1$csection .data%1$c%2$cfilename db %3$cSully_%%d.s%3$c, 0x0%1$c%2$ccmdname db %3$cnasm -f macho64 Sully_%%1$d.s && gcc Sully_%%1$d.o -o Sully_%%1$d && rm Sully_%%1$d.o && ./Sully_%%1$d%3$c, 0x0%1$c%2$cstring db %3$c%5$s%3$c, 0x0%1$c%1$csection .text%1$c%2$cglobal _main%1$c%2$cextern _sprintf%1$c%2$cextern _dprintf%1$c%2$cextern _system%1$c%1$c_main:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%1$c%2$cpush rdi%1$c%2$cpush rsi%1$c%2$cpush rdx%1$c%2$cpush rcx%1$c%2$cpush r8%1$c%2$cpush r12%1$c%2$cpush r13%1$c%1$c%2$cmov r12, %4$d%1$c%1$c%2$ccmp r12d, -1%1$c%2$cje end%1$c%1$c%2$csub rsp, 56%1$c%2$clea rdi, [ rel filebuf ]%1$c%2$clea rsi, [ rel filename ]%1$c%2$cmov rdx, r12%1$c%2$ccall _sprintf%1$c%2$cadd rsp, 56%1$c%1$c%2$csub rsp, 56%1$c%2$clea rdi, [ rel cmdbuf ]%1$c%2$clea rsi, [ rel cmdname ]%1$c%2$cmov rdx, r12%1$c%2$ccall _sprintf%1$c%2$cadd rsp, 56%1$c%1$c%2$clea rdi, [ rel filebuf ]%1$c%2$cmov rsi, 1537%1$c%2$cmov rdx, 0644o%1$c%2$cmov rax, 0x2000005%1$c%2$csyscall%1$c%2$cjc error%1$c%2$cmov r13, rax%1$c%1$c%2$csub rsp, 64%1$c%2$cmov rdi, r13%1$c%2$clea rsi, [ rel string ]%1$c%2$cmov rdx, 0xa%1$c%2$cmov rcx, 0x9%1$c%2$cmov r8, 0x22%1$c%2$cmov r9, r12%1$c%2$cdec r9%1$c%2$cpush rsi%1$c%2$ccall _dprintf%1$c%2$cadd rsp, 64%1$c%1$c%2$cmov rdi, r13%1$c%2$cmov rax, 0x2000006%1$c%2$csyscall%1$c%2$cjc error%1$c%1$c%2$clea rdi, [ rel cmdbuf ]%1$c%2$ccall _system%1$c%2$ccmp eax, -1%1$c%2$cje error%1$c%1$c%2$cpop r13%1$c%2$cpop r12%1$c%2$cpop r8%1$c%2$cpop rcx%1$c%2$cpop rdx%1$c%2$cpop rsi%1$c%2$cpop rdi%1$c%1$cend:%1$c%2$cxor rax, rax%1$c%1$c%2$cleave%1$c%2$cret%1$c%1$cerror:%1$c%2$cmov rax, -1%1$c%1$c%2$cleave%1$c%2$cret%1$c", 0x0
 
 section .text
 	global _main
@@ -71,6 +71,8 @@ _main:
 
 	lea rdi, [ rel cmdbuf ]
 	call _system
+	cmp eax, -1
+	je error
 
 	pop r13
 	pop r12
--- Sully/C/Sully.c
+++ Sully/C/Sully.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 	int fd = 0;
-	int index = 5;
-	char file[10];
-	char cmd[40];
-	char *str = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$c%1$cint main(void)%1$c{%1$c%2$cint fd = 0;%1$c%2$cint index = %5$d;%1$c%2$cchar file[10];%1$c%2$cchar cmd[40];%1$c%2$cchar *str = %3$c%6$s%3$c;%1$c%1$c%2$cif (index < 0)%1$c%2$c%2$creturn 0;%1$C%1$c%2$csprintf(file, %3$cSully_%4$cd.c%3$c, index);%1$c%2$csprintf(cmd, %3$cgcc Sully_%4$c1$d.c -o Sully_%4$c1$d && ./Sully_%4$c1$d%3$c, index);%1$c%1$c%2$cif ((fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)%1$c%2$c%2$creturn -1;%1$c%1$c%2$cdprintf(fd, str, 0xa, 0x9, 0x22, 0x25, index - 1, str);%1$c%2$cclose(fd);%1$c%1$c%2$csystem(cmd);%1$c%1$c%2$creturn 0;%1$c}%1$c";
+	int8_t index = 5;
+	char file[4096] = {0};
+	char cmd[4096] = {0};
+	char *str = "#include <stdio.h>%1$c#include <fcntl.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$c%1$cint main(void)%1$c{%1$c%2$cint fd = 0;%1$c%2$cint8_t index = %5$d;%1$c%2$cchar file[4096] = {0};%1$c%2$cchar cmd[4096] = {0};%1$c%2$cchar *str = %3$c%6$s%3$c;%1$c%1$c%2$cif (index < 0)%1$c%2$c%2$creturn 0;%1$C%1$c%2$csprintf(file, %3$cSully_%4$cd.c%3$c, index);%1$c%2$csprintf(cmd, %3$cgcc Sully_%4$c1$d.c -o Sully_%4$c1$d && ./Sully_%4$c1$d%3$c, index);%1$c%1$c%2$cif ((fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1)%1$c%2$c%2$creturn -1;%1$c%1$c%2$cdprintf(fd, str, 0xa, 0x9, 0x22, 0x25, index - 1, str);%1$c%1$c%2$cif (close(fd) == -1)%1$c%2$c%2$creturn -1;%1$c%1$c%2$cif (system(cmd) == -1)%1$c%2$c%2$creturn -1;%1$c%1$c%2$creturn 0;%1$c}%1$c";
 
 	if (index < 0)
 		return 0;
@@ -21,9 +21,12 @@ int main(void)
 		return -1;
 
 	dprintf(fd, str, 0xa, 0x9, 0x22, 0x25, index - 1, str);
-	close(fd);
 
-	system(cmd);
+	if (close(fd) == -1)
+		return -1;
+
+	if (system(cmd) == -1)
+		return -1;
 
 	return 0;
 }
